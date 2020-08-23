ffea78ac194787be73a31d022589ea834d347b3e
Jose Pedro <pedroosd28@gmail.com>
2020-08-23 01:17:17 -0300

    Add: Adding .gitigonre

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -76,7 +76,8 @@ def meuCV():
 	formacao_ = {'1':formacao('Governador Adauto Bezerra', 'Crato-CE', 'Ensino Medio', 'Concluido - 2019').__dict__,
 				 '2':formacao('Instituto Federal do Ceará', 'Crato-CE', 'Sistemas de Informação', 'Cursando - 2019').__dict__,
 				 '3':formacao('IAExpert', 'Remoto', 'Redes Neurais Artificiais em Python', 'Concluido - 29/06/2020', '80').__dict__,
-				 '4':formacao('Udemy', 'Remoto', 'Python para Data Science e Machine Learning - Completo', 'Concluido - 15/06/2020', '18').__dict__}
+				 '4':formacao('Udemy', 'Remoto', 'Python para Data Science e Machine Learning - Completo', 'Concluido - 15/06/2020', '18').__dict__,
+				 '5':formacao('Udemy', 'Remoto', 'Desenvolva uma aplicação com Django 2.0 e deploy no heroku', 'Concluído - 19/07/2020', '7.5').__dict__}
 
 	emprego_ = {'1': emprego('Ricardo Eletro', 'Assistente Administrativo', '15/08/2019', '15/07/2020', 'Controlava todos os contratos feitos na loja, além de ser um dos responsáveis pelo pós-venda').__dict__}
 
@@ -86,24 +87,35 @@ def meuCV():
 						  'https://github.com/oopaze/Booker').__dict__,
 				'2': projetos('CVCreate',
 							  'Json, Python-Docx, Git',
-							  'Um algoritmo que gera seu curriculo automaticamente',
+							  'Um algoritmo que gera seu curriculo automaticamente a partir de um Json',
 							  'https://github.com/oopaze/CVCreate').__dict__,
-				'3': projetos('FastCurriculo-API',
+				'3': projetos('FastCurriculo',
 							  'Flask, Json, Python-Docx, Git, Heroku',
 							  'Uma API que usa o CVCreate para gerar Curriculos a partir do preenchimento de um form',
-							  'https://github.com/oopaze/FastCurriculo-API').__dict__, 
+							  'https://github.com/oopaze/FastCurriculo').__dict__, 
 				'4': projetos('Sua Compra',
 							  'Tkinter, SQLite, Socket',
 							  'Um sistema de agenciamento de pequenos supermercados',
-							  'https://github.com/oopaze/Sua-Compra').__dict__}
+							  'https://github.com/oopaze/Sua-Compra').__dict__,
+				'5': projetos('MercadinAPI - Em desenvolvimento',
+							  'Python, Flask, PyTest, SQLAlchemy, Marshmallow, Git',
+							  'API de gerenciamento de pequenos mercados',
+							  'https://github.com/oopaze/Mercadin-API').__dict__,
+				'6': projeto('REAnalytics',
+							 'OpenPyXL',
+							 'Sistema de preenchimento automático de planilhas',
+							 'Privado').__dict__}
 
 	habilidades_  = {'1': habilidades('Inglês', 'Avançado').__dict__, 
-					 '2': habilidades('Python', 'Avançado').__dict__,
-					 '3': habilidades('PostgreSQL/MySQL', 'Intermediario').__dict__,
-					 '4': habilidades('SQLAlchemy', 'Avançado').__dict__,
-					 '5': habilidades('Flask', 'Avançado').__dict__}
+					 '2': habilidades('Python', 'Intermediario').__dict__,
+					 '3': habilidades('JavaScripts', 'Intermediario').__dict__,
+					 '4': habilidades('PostgreSQL/MySQL/MongoDB', 'Intermediario').__dict__,
+					 '5': habilidades('SQLAlchemy/Mongoose', 'Avançado').__dict__,
+					 '6': habilidades('Flask', 'Intermediario').__dict__,
+					 '7': habilidades('Django', 'Intermediario').__dict__,
+					 '8': habilidades('Express.js', 'Intermediario').__dict__}
 
-	areaInteresse_ = {"1":"Desenvolvedor Web Back-End"}
+	areaInteresse_ = {"1":"Desenvolvedor Back-End - Python"}
 
 	data = fields(pessoais_, formacao_, emprego_, projetos_, habilidades_, areaInteresse_) 
 	return json.dumps(data, ensure_ascii=False)
